1aae9a7e88a7d302fd440f8a0d18e9d97960042e
Eswar Krishna Maganti <maganti.ek@gmail>
2021-10-18 10:19:19 +0530

    'InitialCommit'

---
+++ server.js
@@ -0,0 +1,13 @@
+import http from "http";
+import products from "./data/products.js";
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Hello World!</h1>");
+  res.end();
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`SERVER Running on PORT ${PORT}`);
+});
