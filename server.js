3930ca468283a0dd1df1d8d9fdb24f66bdbe70ee
Eswar Krishna Maganti <maganti.ek@gmail>
2021-10-20 09:37:16 +0530

    'handlingNotFoundRoute'

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import http from "http";
 import products from "./data/products.js";
 const server = http.createServer((req, res) => {
-  //   res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  //   res.write("<h1>Hello World!</h1>");
-  res.end(JSON.stringify(products));
+  if (req.url === "/api/products" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route Not Found" }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
